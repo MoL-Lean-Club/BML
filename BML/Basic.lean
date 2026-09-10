5da43878dfc6f6827ca9a6e8d059c49134479947
Ronald de Haan <me@ronalddehaan.eu>
2026-09-10 12:38:01 +0200

    Change LICENSE and start definitions of BML

--- BML/Basic.lean
+++
@@ -1,2 +0,0 @@
-def hello := "world"
-def foo := "bar"
---
+++ BML/Def.lean
@@ -0,0 +1,44 @@
+/-
+Copyright (c) 2026 MoL Lean Club. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: see COLLABORATORS.md
+-/
+
+import Mathlib.Data.Set.Basic
+
+/-!
+We start from the basic definitions of modal logic from
+`CSLib.Logic.Modal`.
+-/
+
+/-! # Modal Logic
+
+Modal logic is a logic for reasoning about relational structures, studying statements about
+necessity (`□φ`) and possibility (`◇φ`).
+
+## References
+
+* [P. Blackburn, M. de Rijke, Y. Venema, *Modal Logic*][Blackburn2001]
+-/
+
+namespace BML
+
+/-- A model consists of a relation between worlds `r` and a valuation `v`. -/
+structure Model (World : Type*) (Atom : Type*) where
+  /-- World accessibility relation. -/
+  r : World → World → Prop
+  /-- Valuation of atoms at a world. -/
+  v : World → Atom → Prop
+
+/-- Propositions. -/
+inductive Proposition (Atom : Type u) : Type u where
+  /-- Atomic proposition. -/
+  | atom (p : Atom)
+  /-- Negation. -/
+  | not (φ : Proposition Atom)
+  /-- Conjunction. -/
+  | and (φ₁ φ₂ : Proposition Atom)
+  /-- Possibility. -/
+  | diamond (φ : Proposition Atom)
+
+end BML
