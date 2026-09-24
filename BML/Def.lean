84e244ad61c8cd45e5b6fa0d610e8deb7869b885
Satvik Duddukuru <145278005+SatvikDuddukuru@users.noreply.github.com>
2026-09-24 11:38:56 +0200

    eval_not and eval_and (trivial) (#31)

--- BML/Def.lean
+++ BML/Def.lean
@@ -90,6 +90,14 @@ lemma eval_box : eval M w φ.box ↔ ∀ x, M.r w x → eval M x φ := by
   unfold Proposition.box
   simp only [eval, not_exists, not_and, not_not]
 
+@[simp]
+lemma eval_not : eval M w φ.not ↔ ¬ (eval M w φ) := by
+  rfl
+
+@[simp]
+lemma eval_and : eval M w (φ₁.and φ₂) ↔ eval M w φ₁ ∧ eval M w φ₂ := by
+  rfl
+
 end Proposition
 
 end BML
