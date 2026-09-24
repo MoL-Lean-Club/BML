002fdd8c2352d53138088e97436a17e2191c01e0
Hua Chang Bakker <44873768+huachangb@users.noreply.github.com>
2026-09-24 15:36:48 +0200

    add theorem 2.20

--- BML/Def.lean
+++ BML/Def.lean
@@ -122,9 +122,37 @@ class Bisimulation {World1 World2 : Type} {Atom : Type}
   back : ∀ w1 w2 v2, Z w1 w2 → M2.r w2 v2 → ∃ v1, M1.r w1 v1 ∧ Z v1 v2
 
 
-
-
-
-
+theorem bisimular_worlds_are_modally_equivalent {World1 World2 : Type} {Atom : Type}
+  {M1 : Model World1 Atom} {M2 : Model World2 Atom}
+  (B : Bisimulation M1 M2) (w1 : World1) (w2 : World2) (h : B.Z w1 w2) :
+  ∀ φ, Proposition.eval M1 w1 φ ↔ Proposition.eval M2 w2 φ := by
+  intro φ
+  induction φ generalizing w1 w2 h with
+  | atom p =>
+    exact B.same_atoms w1 w2 h p
+  | not ψ ih =>
+    simp only [Proposition.eval]
+    rw [ih]
+    exact h
+  | and ψ₁ ψ₂ ih₁ ih₂ =>
+    simp only [Proposition.eval]
+    rw [(ih₁ w1 w2 h), (ih₂ w1 w2 h)]
+  | diamond ψ ih =>
+    simp only [Proposition.eval]
+    constructor
+    · intro ltr
+      obtain ⟨v1, hr, hψ⟩ := ltr
+      obtain ⟨v2, hr', hZ⟩ := B.forth w1 w2 v1 h hr
+      use v2
+      constructor
+      · exact hr'
+      · exact (ih v1 v2 hZ).mp hψ
+    · intro rtl
+      obtain ⟨v2, hr, hψ⟩ := rtl
+      obtain ⟨v1, hr', hZ⟩ := B.back w1 w2 v2 h hr
+      use v1
+      constructor
+      · exact hr'
+      · exact (ih v1 v2 hZ).mpr hψ
 
 end BML
