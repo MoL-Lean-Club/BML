c0a83e22dad4b45d5406f2d46c9e3e31cf1d08eb
Moritz Klopstock <moritzklopstock@yahoo.de>
2026-09-24 11:16:06 +0200

    Add eval diamond

--- BML/Def.lean
+++ BML/Def.lean
@@ -70,13 +70,10 @@ lemma eval_or : eval M w (φ₁.or φ₂) ↔ eval M w φ₁ ∨ eval M w φ₂ 
   simp only [eval, not_and, not_not]
   tauto
 
-lemma eval_diamond {World Atom : Type}
-    (M : Model World Atom)
-    (w : World)
-    (φ : Proposition Atom) :
-    eval M w (.diamond φ) ↔
-      ∃ x : World, M.r w x ∧ eval M x φ := by
-  simp only [eval]
+@[simp]
+lemma eval_diamond :
+    eval M w (.diamond φ) ↔ ∃ x : World, M.r w x ∧ eval M x φ := by
+  simp [eval]
 
 def imply : Proposition Atom → Proposition Atom → Proposition Atom
   | φ₁, φ₂ => (φ₁.and φ₂.not).not
